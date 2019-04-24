b807f55244b0e300626241cf6515ecb28ef69e41
wx <wx@example.com>
2019-04-24 09:38:14 +0800

    first commit

---
+++ 截取Url # 参数/getUrlParams.js
@@ -0,0 +1,8 @@
+function getUrlParam(name) {
+        var url = location.href;
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(url);
+        return results == null ? null : decodeURI(results[1]);
+    }
